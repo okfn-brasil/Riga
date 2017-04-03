4110a4df9b62b5bfc877c5a653ded08fb8fbe7cb
Matias Tuduri <Matias.Tuduri@VERIFONE.com>
2017-04-03 17:10:52 -0300

    Added jquery validations

---
+++ source/js/submitPaper.js
@@ -0,0 +1,41 @@
+$(document).ready(function() {
+    var submitted = false;
+
+    $("#paper_submit_form").validate({
+        rules: {
+            "entry.962917036": { required: true }, // Nomb Paper
+            "entry.1668992231": { // link
+                required: true,
+                url: true
+            },
+            "entry.1559425319": { url: true }, // Github
+            "entry.1607377555": { url: true }, //Datos relacionados
+            "entry.628856331": { url: true }, // Titulo datos relacionados
+            "entry.1372654494": { required: true } //abstract
+        },
+        messages: {
+            "entry.962917036": "Ingrese el nombre del artículo", // Nomb Paper
+            "entry.1668992231": { // link
+                required: "Ingrese el nombre del artículo",
+                url: "Ingrese una URL válida"
+            },
+            "entry.1559425319": { url: "Ingrese una URL válida" }, // Github
+            "entry.1607377555": { url: "Ingrese una URL válida" }, //Datos relacionados
+            "entry.628856331": { url: "Ingrese una URL válida" }, // Titulo datos relacionados
+            "entry.1372654494": { required: "Ingrese el abstract del artículo" }, //abstract
+        }
+    });
+
+    $("#paper_submit_form").on("submit", function(e) {
+        if ($("#paper_submit_form").valid()){
+            submitted = true;
+            window.location='./form-confirmation.html';
+        }
+        else{
+        	e.preventDefault();
+        	e.stopPropagation();
+        }
+
+    });
+
+});
