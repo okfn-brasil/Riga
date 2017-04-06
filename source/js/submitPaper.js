206e75d656590d4b7b086980a8fe30952d390429
mtuduri <tuduri.matias@gmail.com>
2017-04-05 22:01:13 -0300

    fixed data consistency

--- source/js/submitPaper.js
+++ source/js/submitPaper.js
@@ -7,22 +7,14 @@ $(document).ready(function() {
             "entry.1668992231": { // link
                 required: true,
                 url: true
-            },
-            "entry_1559425319": { url: true }, // Github
-            "entry.1607377555": { url: true }, //Datos relacionados
-            "entry_1607377555": { url: true }, // Titulo datos relacionados
-            "entry_1372654494": { required: true } //abstract
+            }
         },
         messages: {
             "entry.962917036": "Ingrese el nombre del artículo", // Nomb Paper
             "entry.1668992231": { // link
                 required: "Ingrese el nombre del artículo",
                 url: "Ingrese una URL válida"
-            },
-            "entry_1559425319": { url: "Ingrese una URL válida" }, // Github
-            "entry_1607377555": { url: "Ingrese una URL válida" }, //Datos relacionados
-            "entry_628856331": { url: "Ingrese una URL válida" }, // Titulo datos relacionados
-            "entry_1372654494": { required: "Ingrese el abstract del artículo" }, //abstract
+            }
         }
     });
 
